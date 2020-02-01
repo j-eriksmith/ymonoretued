325e54b28e8f5113432e231e83659250919ab103
Jake <jacobes1@uci.edu>
2020-02-01 12:44:34 -0800

    Sand metafiles. QTE code. New inputs

---
+++ Assets/Scripts/QTE/BaseQTE.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseQTE : MonoBehaviour
+{
+    protected Transform blacksmithCenter;
+    protected Transform blacksmithLeft;
+
+    protected float timeOfInitialization;
+    protected float timeSinceInitialization;
+    protected GameObject activatingPlayer; // Todo: should be the Player component
+
+    public void Start()
+    {
+        Initialize();
+    }
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("A"))
+        {
+            ReceiveInput("A");
+        }
+        if (Input.GetButtonDown("B"))
+        {
+            ReceiveInput("B");
+        }
+        if (Input.GetButtonDown("X"))
+        {
+            ReceiveInput("X");
+        }
+        if (Input.GetButtonDown("Y"))
+        {
+            ReceiveInput("Y");
+        }
+    }
+
+    public virtual void Initialize()
+    {
+        timeOfInitialization = Time.time;
+        DisplayGraphics();
+    }
+
+    public virtual void ReceiveInput(string buttonPressed)
+    {
+        timeSinceInitialization = Time.time - timeOfInitialization;
+    }
+
+    protected abstract void DisplayGraphics();
+}
---
+++ Assets/Scripts/QTE/SequenceQTE.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceQTE : BaseQTE
+{
+    public int minSequenceLength;
+    public int maxSequenceLength;
+
+    private string[] validEvents = {"A", "B", "X", "Y"};
+    private Queue<string> inputSequence = new Queue<string>();
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        int eventsToGenerate = Mathf.FloorToInt(Random.Range(minSequenceLength, maxSequenceLength + 1));
+        for (int i = 0; i < eventsToGenerate; ++i)
+        {
+            string eventToEnqueue = validEvents[Mathf.FloorToInt(Random.Range(0, validEvents.Length))];
+            inputSequence.Enqueue(eventToEnqueue);            
+            Debug.Log(eventToEnqueue);
+        }
+    }
+
+    public override void ReceiveInput(string buttonPressed)
+    {
+        Debug.Log(buttonPressed);
+        base.ReceiveInput(buttonPressed);
+
+        if (buttonPressed == inputSequence.Peek())
+        {
+            inputSequence.Dequeue();
+            // TODO(smith): Hide the current button sprite
+
+            if (inputSequence.Count == 0)
+            {
+                // TODO(smith): Call Player.QTESucceed()
+                Debug.Log("Finished QTE!");
+            }
+        }
+        // No else case for now ... gotta finish this game
+    }
+
+    protected override void DisplayGraphics()
+    {
+        
+    }
+}
