1fff3c9e455e662238759f0625883710699b07f5
Jack VanDrunen <jackvandrunen@gmail.com>
2020-02-01 17:23:12 -0800

    Enemy logic class hierarchy

--- Assets/Scripts/Enmity.cs
+++ Assets/Scripts/Enmity.cs
@@ -2,17 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enmity : MonoBehaviour
+public abstract class Enmity : MonoBehaviour
 {
+    public float cooldownTime;
+    public float range;
+
+    protected float cooldown;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Enemy spawned!");
+        cooldown = cooldownTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        cooldown -= Time.deltaTime;
+        Orient();
+        Move();
+        Attack();
+    }
+
+    protected virtual void Orient()
+    {
 
     }
+
+    protected virtual bool InRange()
+    {
+        // distance between me and player is less than `range`
+        return false;
+    }
+
+    // should call InRange to determine how to move (speed up/slow down/hold)
+    protected abstract void Move();
+
+    // should call InRange to determine whether to attack
+    // should use `cooldown`
+    protected abstract void Attack();
 }
