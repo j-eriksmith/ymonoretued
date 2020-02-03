4538c30ea8721794be3ff3e196d82fa96a5d9802
Christopher DiPalma <cdipalma@uci.edu>
2020-02-02 16:27:08 -0800

    FIREBALL MOVES, SP0DER ANIM FIX

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.right * 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
